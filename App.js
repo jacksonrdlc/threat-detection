19a6f974f2060ce8eadd9e030935d1537a64462c
Jack Rudelic <jackrudelic@gmail.com>
2018-06-07 13:17:11 -0500

    images

--- App.js
+++ App.js
@@ -19,13 +19,13 @@ export default class App extends React.Component {
 
   getFrames() {
     const axiosInstance = axios.create({
-      baseURL: "https://zk5vbj5avk.execute-api.us-east-1.amazonaws.com/development",
-      headers: { 'X-api-key': "J9gDvQBdt25qXvCWk5Oou7HB1IdnJaqk5YYKdvfA" }
+      baseURL: "https://ere8in5vtf.execute-api.us-east-1.amazonaws.com/development",
+      headers: { 'X-api-key': "xBCYm2iB5W6GfgUKycClz8gcYBVzhizs6g8uRVn8", 'Content-Type': 'application/json' }
     });
     return axiosInstance.get('enrichedframe')
       .then((responseJson) => {
         let hits = responseJson.data
-        console.log(hits)
+        console.log(responseJson)
         this.setState({ hits });
       })
       .catch((error) => {
@@ -34,26 +34,52 @@ export default class App extends React.Component {
   }
 
   renderFrames(frame) {
-    return (
-      <View>
-        <Image
-          style={{width: '100%', height: 200}}
-          source={{uri: frame.item.s3_presigned_url}}
-        />
-        <FlatList
-          style={styles.frame}
-          data={frame.item.rekog_labels}
-          renderItem={(label) => {
-            return (
-              <View>
-                <Text>{label.item.Name}</Text>
-                <Text>{label.item.Confidence}</Text>
-              </View>);
-          }}
-          keyExtractor={frame => frame.index}
-        />
-      </View>
-    );
+    if (frame.item.warning_response.name) {
+      return (
+        <View
+          style={{ margin: 10, backgroundColor: '#BF5144', borderRadius: 4, shadowColor: '#000', shadowOpacity: .15, shadowOffset: {width: 1, height: 3} }}>
+          <Image
+            style={{ width: 350, height: 200, margin: 20 }}
+            source={require('./images/Rudelic_Jack.jpeg')}
+          />
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', padding: 10, backgroundColor: '#AF493D', color: '#fff' }}>
+            <Text style={{ fontSize: 22, color: '#fff' }}>Threat Detected</Text>
+            <Text style={{ fontSize: 16, color: '#fff' }}>Jack Rudelic</Text>
+            <Text style={{ fontSize: 16, color: '#fff' }}>{ new Date().toLocaleString()}</Text>
+          </View>
+        </View>
+      );
+    } else if (frame.item.image_response.name) {
+      return (
+        <View
+          style={{ margin: 10, backgroundColor: '#8CBEA3', borderRadius: 4, shadowColor: '#000', shadowOpacity: .15, shadowOffset: {width: 1, height: 3} }}>
+          <Image
+            style={{ width: 350, height: 200, margin: 20 }}
+            source={require('./images/DawnKing.jpg')}
+          />
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', padding: 10, backgroundColor: '#75B693', color: '#fff' }}>
+            <Text style={{ fontSize: 22, color: '#fff' }}>Friend Detected</Text>
+            <Text style={{ fontSize: 16, color: '#fff' }}>Dawn King</Text>
+            <Text style={{ fontSize: 16, color: '#fff' }}>{ new Date().toLocaleString()}</Text>
+          </View>
+        </View>
+      );
+    } else {
+      return (
+        <View
+          style={{ margin: 10, backgroundColor: '#ccc', borderRadius: 4, shadowColor: '#000', shadowOpacity: .15, shadowOffset: {width: 1, height: 3} }}>
+          <Image
+            style={{ width: 350, height: 200, margin: 20 }}
+            source={require('./images/Unknown_Person.png')}
+          />
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', padding: 10, backgroundColor: '#aaa', color: '#fff' }}>
+            <Text style={{ fontSize: 22, color: '#fff' }}>Unknown Persons</Text>
+            <Text style={{ fontSize: 16, color: '#fff' }}>Face May Be Covered</Text>
+            <Text style={{ fontSize: 16, color: '#fff' }}>{ new Date().toLocaleString()}</Text>
+          </View>
+        </View>
+      );
+    }
   }
 
   render() {
@@ -72,16 +98,24 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EEEEEE',
     alignItems: 'stretch',
     justifyContent: 'center',
   },
-  frame: {
+  frameGood: {
     flex: 1,
-    backgroundColor: '#eee',
-    color: 'white',
+    color: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 20,
+    backgroundColor: '#76B594',
+    padding: 10
+  },
+  frameBad: {
+    flex: 1,
+    color: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#AF493D',
+    padding: 10
   },
 });
